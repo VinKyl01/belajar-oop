d4829b085df26a9486d91c0271af2b54d3b1dfcb
VinKyl01 <clauth321@gmail.com>
2026-08-09 09:05:16 +0700

    first commit

--- Tugas_OOP.php
+++ Tugas_OOP.php
@@ -1,34 +1,34 @@
 <?php
-class Laptop {
-    // Property
+class Laptop{
+    // property
     public $merek;
     public $warna;
     public $ram;
 
-    // 
-    public function __construct($merek,$warna,$ram) {
+    // construct
+    public function __construct($merek,$warna,$ram){
         $this->merek = $merek;
         $this->warna = $warna;
         $this->ram = $ram;
     }
 
-    // Method
+    // method
     public function tampilkanData(){
         echo "===DATA LAPTOP===<br>";
-        echo "Merek :". $this->merek."<br>";
-        echo "Warna :". $this->warna."<br>";
-        echo "Ram :". $this->ram."GB<br>";
+        echo "Merek :". $this->merek. "<br>";
+        echo "Warna :". $this->warna. "<br>";
+        echo "RAM :". $this->ram. "GB<br>";
     }
 
     public function nyalakan(){
-        echo "<br>Laptop {$this->merek} Berhasil Dinyalakan.";
+        echo "<br>Laptop {$this->merek} berhasil dinyalakan.";
     }
 }
 
-// Object
-$laptop1 = new Laptop("ASUS", "HITAM", 16);
+// object
+$laptop1 = new Laptop("ASUS","HITAM", 16);
 
-// memanggil method
+// Memanggil Method
 $laptop1->tampilkanData();
 $laptop1->nyalakan();
 ?>
